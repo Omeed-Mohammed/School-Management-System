701508b4db65b688d61dbf746996e988014fdd2f
omeed mohammed <omeed1987@gmail.com>
2025-10-05 01:45:33 +0200

    Added custom controls (Button, TextBox, DateTimePicker, RadioButton)

---
+++ Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace School_Management_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.rjButton1 = new School_Management_System.ToolsControls.RJButton();
+            this.customTextBox1 = new School_Management_System.ToolsControls.CustomTextBox();
+            this.customDateTimePicker1 = new School_Management_System.ToolsControls.CustomDateTimePicker();
+            this.customRadioButton3 = new School_Management_System.ToolsControls.CustomRadioButton();
+            this.customRadioButton2 = new School_Management_System.ToolsControls.CustomRadioButton();
+            this.customRadioButton1 = new School_Management_System.ToolsControls.CustomRadioButton();
+            this.customTextBox3 = new School_Management_System.ToolsControls.CustomTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(45, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(356, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "School Management System";
+            // 
+            // rjButton1
+            // 
+            this.rjButton1.BackColor = System.Drawing.Color.SteelBlue;
+            this.rjButton1.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.rjButton1.BorderRadius = 10;
+            this.rjButton1.BorderSize = 1;
+            this.rjButton1.DisabledBackColor = System.Drawing.Color.Gray;
+            this.rjButton1.FlatAppearance.BorderSize = 0;
+            this.rjButton1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.rjButton1.ForeColor = System.Drawing.Color.White;
+            this.rjButton1.Location = new System.Drawing.Point(113, 463);
+            this.rjButton1.Name = "rjButton1";
+            this.rjButton1.NormalBackColor = System.Drawing.Color.MediumSlateBlue;
+            this.rjButton1.PressedBackColor = System.Drawing.Color.DarkSlateBlue;
+            this.rjButton1.Size = new System.Drawing.Size(211, 61);
+            this.rjButton1.TabIndex = 6;
+            this.rjButton1.Text = "rjButton1";
+            this.rjButton1.UseVisualStyleBackColor = false;
+            // 
+            // customTextBox1
+            // 
+            this.customTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.customTextBox1.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.customTextBox1.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.customTextBox1.BorderRadius = 10;
+            this.customTextBox1.BorderSize = 2;
+            this.customTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customTextBox1.ForeColor = System.Drawing.Color.DimGray;
+            this.customTextBox1.Location = new System.Drawing.Point(72, 325);
+            this.customTextBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.customTextBox1.Multiline = false;
+            this.customTextBox1.Name = "customTextBox1";
+            this.customTextBox1.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.customTextBox1.PasswordChar = false;
+            this.customTextBox1.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.customTextBox1.PlaceholderText = "User Name";
+            this.customTextBox1.Size = new System.Drawing.Size(250, 33);
+            this.customTextBox1.TabIndex = 5;
+            this.customTextBox1.Texts = "";
+            this.customTextBox1.UnderlinedStyle = true;
+            // 
+            // customDateTimePicker1
+            // 
+            this.customDateTimePicker1.BorderColor = System.Drawing.Color.LavenderBlush;
+            this.customDateTimePicker1.BorderSize = 0;
+            this.customDateTimePicker1.CalendarMonthBackground = System.Drawing.SystemColors.InactiveBorder;
+            this.customDateTimePicker1.CalendarTrailingForeColor = System.Drawing.Color.Linen;
+            this.customDateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customDateTimePicker1.Location = new System.Drawing.Point(72, 267);
+            this.customDateTimePicker1.MinimumSize = new System.Drawing.Size(4, 35);
+            this.customDateTimePicker1.Name = "customDateTimePicker1";
+            this.customDateTimePicker1.Size = new System.Drawing.Size(280, 35);
+            this.customDateTimePicker1.SkinColor = System.Drawing.Color.Transparent;
+            this.customDateTimePicker1.TabIndex = 4;
+            this.customDateTimePicker1.TextColor = System.Drawing.Color.SteelBlue;
+            // 
+            // customRadioButton3
+            // 
+            this.customRadioButton3.AutoSize = true;
+            this.customRadioButton3.BackColor = System.Drawing.Color.SteelBlue;
+            this.customRadioButton3.CheckedColor = System.Drawing.Color.MidnightBlue;
+            this.customRadioButton3.ForeColor = System.Drawing.Color.White;
+            this.customRadioButton3.Location = new System.Drawing.Point(72, 230);
+            this.customRadioButton3.MinimumSize = new System.Drawing.Size(0, 21);
+            this.customRadioButton3.Name = "customRadioButton3";
+            this.customRadioButton3.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.customRadioButton3.Size = new System.Drawing.Size(134, 21);
+            this.customRadioButton3.TabIndex = 3;
+            this.customRadioButton3.TabStop = true;
+            this.customRadioButton3.Text = "customRadioButton3";
+            this.customRadioButton3.UnCheckedColor = System.Drawing.Color.White;
+            this.customRadioButton3.UseVisualStyleBackColor = false;
+            // 
+            // customRadioButton2
+            // 
+            this.customRadioButton2.AutoSize = true;
+            this.customRadioButton2.BackColor = System.Drawing.Color.SteelBlue;
+            this.customRadioButton2.CheckedColor = System.Drawing.Color.MidnightBlue;
+            this.customRadioButton2.ForeColor = System.Drawing.Color.White;
+            this.customRadioButton2.Location = new System.Drawing.Point(72, 203);
+            this.customRadioButton2.MinimumSize = new System.Drawing.Size(0, 21);
+            this.customRadioButton2.Name = "customRadioButton2";
+            this.customRadioButton2.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.customRadioButton2.Size = new System.Drawing.Size(134, 21);
+            this.customRadioButton2.TabIndex = 2;
+            this.customRadioButton2.TabStop = true;
+            this.customRadioButton2.Text = "customRadioButton2";
+            this.customRadioButton2.UnCheckedColor = System.Drawing.Color.White;
+            this.customRadioButton2.UseVisualStyleBackColor = false;
+            // 
+            // customRadioButton1
+            // 
+            this.customRadioButton1.AutoSize = true;
+            this.customRadioButton1.BackColor = System.Drawing.Color.SteelBlue;
+            this.customRadioButton1.CheckedColor = System.Drawing.Color.MidnightBlue;
+            this.customRadioButton1.ForeColor = System.Drawing.Color.White;
+            this.customRadioButton1.Location = new System.Drawing.Point(72, 176);
+            this.customRadioButton1.MinimumSize = new System.Drawing.Size(0, 21);
+            this.customRadioButton1.Name = "customRadioButton1";
+            this.customRadioButton1.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.customRadioButton1.Size = new System.Drawing.Size(134, 21);
+            this.customRadioButton1.TabIndex = 1;
+            this.customRadioButton1.TabStop = true;
+            this.customRadioButton1.Text = "customRadioButton1";
+            this.customRadioButton1.UnCheckedColor = System.Drawing.Color.White;
+            this.customRadioButton1.UseVisualStyleBackColor = false;
+            // 
+            // customTextBox3
+            // 
+            this.customTextBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.customTextBox3.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.customTextBox3.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.customTextBox3.BorderRadius = 10;
+            this.customTextBox3.BorderSize = 2;
+            this.customTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customTextBox3.ForeColor = System.Drawing.Color.DimGray;
+            this.customTextBox3.Location = new System.Drawing.Point(72, 365);
+            this.customTextBox3.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.customTextBox3.Multiline = false;
+            this.customTextBox3.Name = "customTextBox3";
+            this.customTextBox3.Padding = new System.Windows.Forms.Padding(14, 8, 14, 8);
+            this.customTextBox3.PasswordChar = true;
+            this.customTextBox3.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.customTextBox3.PlaceholderText = "Password";
+            this.customTextBox3.Size = new System.Drawing.Size(252, 35);
+            this.customTextBox3.TabIndex = 8;
+            this.customTextBox3.Texts = "";
+            this.customTextBox3.UnderlinedStyle = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.SteelBlue;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "text1",
+            "text1",
+            "text1",
+            "text1",
+            "text1"});
+            this.comboBox1.Location = new System.Drawing.Point(70, 114);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(182, 24);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(450, 600);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.customTextBox3);
+            this.Controls.Add(this.rjButton1);
+            this.Controls.Add(this.customTextBox1);
+            this.Controls.Add(this.customDateTimePicker1);
+            this.Controls.Add(this.customRadioButton3);
+            this.Controls.Add(this.customRadioButton2);
+            this.Controls.Add(this.customRadioButton1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private ToolsControls.CustomRadioButton customRadioButton1;
+        private ToolsControls.CustomRadioButton customRadioButton2;
+        private ToolsControls.CustomRadioButton customRadioButton3;
+        private ToolsControls.CustomDateTimePicker customDateTimePicker1;
+        private ToolsControls.CustomTextBox customTextBox1;
+        private ToolsControls.RJButton rjButton1;
+        private ToolsControls.CustomTextBox customTextBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using School_Management_System.Register_Login;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace School_Management_System
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+           // Application.Run(new frmLogin());
+        }
+    }
+}
